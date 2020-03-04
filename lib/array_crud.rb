ded1c667386751944f95bbb90351ac29118edb0a
Marsha Annon <marshaannon@gmail.com>
2020-03-04 17:59:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,13 +4,11 @@ end
 
 def create_an_array
   ["marigold", "dahlia", "lilac", "gardenia"]
-
 end
 
-def add_element_to_end_of_array(array, iris)
+def add_element_to_end_of_array(array, element)
   array = ["marigold", "dahlia", "lilac", "gardenia"]
-  add_element_to_end_of_array << "iris"
-
+  element << "iris"
 end
 
 def add_element_to_start_of_array(array, element)
